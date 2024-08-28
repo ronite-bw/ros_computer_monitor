5ebca6340baf134c43b18d8cf6e5cadbdbdbb565
roni <roni.te@bluewhite.ai>
2024-08-28 16:32:30 +0300

    Adjust to Cramim

--- ros_top/src/ros_top/ros_top.py
+++ ros_top/src/ros_top/ros_top.py
@@ -1,23 +1,16 @@
 import rclpy
 from rclpy.node import Node
 from computer_msgs.msg import Process, CPUUsage
+from std_msgs.msg import Float32, UInt64
 
 import os
 import pathlib
 import psutil
 
-NODE_FIELDS = ['pid', 'get_cpu_percent', 'get_memory_percent', 'get_num_threads']
-
 class ROSTop(Node):
 
     def __init__(self):
-        super().__init__('ros_top')
-        self.pub = self.create_publisher(CPUUsage, '/cpu_usage', 1)
-        self.declare_parameter('workspace', str(pathlib.Path('~/ros2_ws').expanduser()))
-
-        distro = os.environ['ROS_DISTRO']
-        self.prefixes = [f'/opt/ros/{distro}', self.get_parameter('workspace').value]
-
+        super().__init__('cpu_monitor')
         self.processes = {}
 
         self.poll()
@@ -30,17 +23,29 @@ class ROSTop(Node):
 
         freq = self.get_parameter('pub_frequency').value
         self.pub_timer = self.create_timer(1.0 / freq, self.publish)
+        
+        self.declare_parameter('ignored_nodes', [''])
+        self.ignored_node_names = self.get_parameter('ignored_nodes').value
+        print(self.ignored_node_names)
+        for n in self.ignored_node_names:
+            print("\t\tWill ignore node {}".format(n))
+
+        self.total_cpu_publisher =  self.create_publisher(Float32, "cpu_monitor/total_cpu", 20)
+        self.total_free_mem_publisher =  self.create_publisher(UInt64, "cpu_monitor/total_free_mem", 20)
+        
+        # core frequency
+        self.core_publishers = []
+        self.freq_publishers = []
+        for i in range(psutil.cpu_count()):
+            self.core_publishers.append(self.create_publisher(Float32, "cpu_monitor/core" + str(i), 20))
+            self.freq_publishers.append(self.create_publisher(Float32, "cpu_monitor/freq" + str(i), 20))
+
 
     def is_ros(self, process):
-        if process.info['name'] == 'ros2':
-            return False
-        keys = process.info['cmdline'][:2] + [process.info['exe']]
-        for key in keys:
-            if not key:
-                return False
-            for prefix in self.prefixes:
-                if key.startswith(prefix):
-                    return True
+        if '__node' in ' '.join(process.info['cmdline']):
+            return True
+        return False
+
 
     def poll(self, event=None):
         for p in psutil.process_iter(['name', 'cmdline', 'exe']):
@@ -48,18 +53,47 @@ class ROSTop(Node):
                 self.processes[p.pid] = p
 
     def publish(self, event=None):
-        cpu = CPUUsage()
+        #Publish the total CPU used
+        total_cpu_msg = Float32()
+        total_cpu_msg.data = psutil.cpu_percent()
+        self.total_cpu_publisher.publish(total_cpu_msg)
+
+        #Publish free memory
+        vm = psutil.virtual_memory()
+        total_free_mem_msg = UInt64()
+        total_free_mem_msg.data = getattr(vm, 'free')
+        self.total_free_mem_publisher.publish(total_free_mem_msg)
+        
+        #Publish cpu percent and freq per core
+        cpu_percent_per_core = psutil.cpu_percent(percpu=True)
+        cpu_freq_per_core = psutil.cpu_freq(percpu=True)
+        cpu_percent_per_core_msg = Float32()
+        cpu_freq_per_core_msg = Float32()
+        
+        for i in range(psutil.cpu_count()):
+            cpu_percent_per_core_msg.data = cpu_percent_per_core[i]
+            cpu_freq_per_core_msg.data = cpu_freq_per_core[i][0]
+            self.core_publishers[i].publish(cpu_percent_per_core_msg)
+            if cpu_freq_per_core is not None and len(cpu_freq_per_core):
+                self.freq_publishers[i].publish(cpu_freq_per_core_msg)
+
+        #Publish process info with a different topic
         for pid, process in list(self.processes.items()):
             try:
-                p = Process()
-                p.pid = pid
-                p.command = ' '.join(process.cmdline())
+                pub_process = Process()
+                pub_process.pid = pid
+                pub_process.command = ' '.join(process.cmdline())
                 for field in ['name', 'cpu_percent', 'memory_percent', 'num_threads']:
-                    setattr(p, field, getattr(process, field)())
-                cpu.processes.append(p)
+                    setattr(pub_process, field, getattr(process, field)())
+                if pub_process.name == 'ros_top' or pub_process.name in self.ignored_node_names:
+                    continue
+                topic_name = '/cpu_monitor/' + pub_process.name + '/cpu'
+                temp_pub = self.create_publisher(Float32, topic_name, 20)
+                cpu_percent_msg = Float32()
+                cpu_percent_msg.data = pub_process.cpu_percent
+                temp_pub.publish(cpu_percent_msg)
             except psutil.NoSuchProcess:
                 del self.processes[pid]
-        self.pub.publish(cpu)
 
 def main(args=None):
     rclpy.init(args=args)
